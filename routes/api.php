e163f0ed22eeee0c90ec52a123281fade5e19269
RanieryDutra <raniery_dutra@yahoo.com.br>
2023-11-07 23:09:59 -0300

    Testando os commits

--- routes/api.php
+++ routes/api.php
@@ -7,7 +7,8 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
+|   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
+
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider and all of them will
 | be assigned to the "api" middleware group. Make something great!
